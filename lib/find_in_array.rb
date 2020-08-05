d10653364f674f953d13626805d9cb7f36369f0a
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-05 03:09:05 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,6 +2,8 @@ def find_element_index(array, value_to_find)
   count=0 
   while count<array.length do 
     puts array[count]==value_to_find
+      return count
+    end
     count+=1 
   end 
 end
